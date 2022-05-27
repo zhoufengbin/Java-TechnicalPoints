7bd45311f062b1e2ed3ca3724a59acd0bc7432c9
zhoufengbin <zfb2264@163.com>
2022-05-27 14:52:28 +0800

    ThreadPoolExecutor

---
+++ TheardPoolExecutor/src/main/java/com/zhoufengbin/TestTheadPoolExecutor.java
@@ -0,0 +1,12 @@
+package com.zhoufengbin;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class TestTheadPoolExecutor {
+
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor();
+
+    }
+}
